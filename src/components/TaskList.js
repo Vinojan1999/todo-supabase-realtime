ea756ea1bc7500a6ea5397ba9c5a9612109887b0
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-07-18 19:16:14 +0530

    Listed all the changes in realtime

---
+++ src/components/TaskList.js
@@ -0,0 +1,106 @@
+import { Text, HStack, VStack, Image, Box, StackDivider } from '@chakra-ui/react';
+import ClearTask from './ClearTask';
+import DeleteTask from './DeleteTask';
+import img from '../images/empty.svg';
+
+import { useState ,useEffect } from 'react';
+import { supabase } from '../supabase'
+// import { useRealtime } from 'react-supabase';
+
+export default function TaskList() {
+    // Using react-supabase
+    // const [result, reexecute] = useRealtime('todos');
+    // const { data: tasks, error, fetching } = result;
+
+    const [tasks, setTasks] = useState([]);
+
+    async function fetchData() {
+        let { data: tasks, error } = await supabase.from('todos').select('*')
+        if (error) {
+            console.error('Error fetching tasks:', error);
+        } else {
+            setTasks(tasks);
+        }
+        console.log(tasks);
+    }
+
+    // useEffect(() => {
+    //   fetchData();
+    // }, [])
+
+    useEffect(() => {
+        
+        fetchData();
+
+        const todos = supabase
+            .channel('*')
+            .on(
+                'postgres_changes',
+                { 
+                    event: '*', 
+                    schema: 'public', 
+                    table: 'todos' 
+                },
+                (payload) => {
+                    const newTasks = payload.new;
+                    const oldTask = payload.old;
+                    const event = payload.eventType;
+                    console.log(event);
+
+                    if (event === 'INSERT') {
+                        setTasks((prevTasks) => [...prevTasks, newTasks]);
+                    } else if (event === 'UPDATE') {
+                        setTasks((prevTasks) => {
+                            prevTasks.map((task) => 
+                                task.id === oldTask.id ? newTasks : task
+                            )
+                        });
+                    } else if (event === 'DELETE') {
+                        setTasks((prevTasks) =>
+                            prevTasks.filter((task) => task.id !== oldTask.id)
+                        );
+                    }
+                }
+            )
+            .subscribe()
+
+        return () => {
+            supabase.removeChannel(todos);
+        }
+    }, [])
+    
+    
+    if (!tasks || !tasks.length) {
+        return (
+            <Box alignItems="center">
+                <Image src={img} mt="30px" maxW="95%" />
+            </Box>
+        );
+    }
+
+    return (
+        <>
+            <VStack
+                divider={<StackDivider />}
+                borderColor="gray.700"
+                borderWidth="2px"
+                p="5"
+                borderRadius="lg"
+                w="100%"
+                maxW={{ base: '90vw', sm: '80vw', lg: '50vw', xl: '30vw' }}
+                alignItems="stretch"
+            >
+                {tasks.map((task) => (
+                    <HStack key={task.id}>
+                        <Text w="100%" p="8px" borderRadius="lg">
+                            {task.text}
+                        </Text>
+                        <DeleteTask id={task.id} />
+                    </HStack>
+                ))}
+            </VStack>
+
+            <ClearTask />
+        </>
+    )
+}
